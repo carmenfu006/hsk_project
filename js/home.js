09812207df7f642a6b08da9566e1d293eceb08bd
carmenfu006 <guess_006@hotmail.com>
2023-09-27 11:47:01 +0800

    Create FAQs for homepage

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
 $("#home-menu-btn").addClass('active');
+
+$(".collapse")
+.on("show.bs.collapse", function () {
+  $(this)
+    .prev(".card-header")
+    .find(".fa")
+    .removeClass("fa-plus")
+    .addClass("fa-minus");
+})
+.on("hide.bs.collapse", function () {
+  $(this)
+    .prev(".card-header")
+    .find(".fa")
+    .removeClass("fa-minus")
+    .addClass("fa-plus");
+});
