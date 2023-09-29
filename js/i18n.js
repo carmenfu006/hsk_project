1dccdff71f229851046f44ddaa3f4f679dbd3351
carmenfu006 <guess_006@hotmail.com>
2023-09-29 17:21:17 +0800

    Translate index page

--- js/i18n.js
+++ js/i18n.js
@@ -1,17 +1,154 @@
 setQueryString()
 
-translateWeb('.i18n-1', '首页', 'Home', 'Beranda', 'الصفحة الرئيسية')
-translateWeb('.i18n-2', 'HSK口语移动端考期', 'HSK Spoken Mobile Test Dates', 'Jadwal Ujian HSK Lisan Versi Seluler', 'فترة الامتحان لاختبار HSK الشفوي عبر الهاتف المحمول')
-translateWeb('.i18n-3', '考点与学校合作', 'Test Centers and School Partnerships', 'Kerjasama dengan Lokasi Ujian dan Sekolah', 'التعاون مع مراكز الاختبار والمدارس')
-translateWeb('.i18n-4', '最新资讯', 'Latest Information', 'Informasi Terbaru', 'أحدث المعلومات')
-translateWeb('.i18n-5', '常见问题', 'FAQs', 'Pertanyaan Umum', 'الأسئلة الشائعة')
-translateWeb('.i18n-6', '考生登入/注册', 'Candidate Login/Register', 'Masuk/Pendaftaran Peserta Ujian', 'تسجيل الطلاب / تسجيل الدخول')
-translateWeb('.i18n-7', '合作方登入', 'Partner Login', 'Masuk Mitra', 'تسجيل الشريك')
-translateWeb('.i18n-8', '全球唯一HSK口语移动端考试', 'The only HSK Spoken Mobile Test available worldwide', 'Satu-satunya Ujian HSK Lisan Versi Seluler di Dunia', 'اختبار HSK الشفوي للهواتف المحمولة الوحيد في العالم')
-translateWeb('.i18n-9', '联系我们: ', 'Contact Us: ', 'Hubungi Kami: ', 'تواصل معنا: ')
-translateWeb('.i18n-10', '友情链接: ', 'Friendly Links: ', 'Tautan Sahabat: ', 'روابط صديقة: ')
-translateWeb('.i18n-11', '更多质讯: ', 'More Information: ', 'Informasi Lebih Lanjut: ', 'لمزيد من المعلومات: ')
-translateWeb('.i18n-12', 'HSK口语移动端考试App下载', 'Download the HSK Spoken Mobile Test App', 'Unduh Aplikasi Ujian HSK Lisan Versi Seluler', 'تنزيل تطبيق اختبار HSK الشفوي للهواتف المحمولة')
+translateWeb(
+  '.i18n-1', 
+  '首页', 
+  'Home', 
+  'Beranda', 
+  'الصفحة الرئيسية'
+)
+translateWeb(
+  '.i18n-2', 
+  'HSK口语移动端考期', 
+  'HSK Spoken Mobile Test Dates', 
+  'Tanggal Ujian', 
+  'فترة الامتحان لاختبار HSK الشفوي عبر الهاتف المحمول'
+)
+translateWeb(
+  '.i18n-3', 
+  '考点与学校合作', 
+  'Partnerships', 
+  'Kerjasama', 
+  'التعاون مع مراكز الاختبار والمدارس'
+)
+translateWeb(
+  '.i18n-4', '最新资讯', 
+  'Latest Information', 
+  'Informasi Terbaru', 
+  'أحدث المعلومات'
+)
+translateWeb(
+  '.i18n-5', 
+  '常见问题', 
+  'FAQs', 
+  'Pertanyaan Umum', 
+  'الأسئلة الشائعة'
+)
+translateWeb(
+  '.i18n-6', 
+  '考生登入/注册', 
+  'Candidate Login/Register', 
+  'Masuk/Pendaftaran Peserta Ujian', 
+  'تسجيل الطلاب / تسجيل الدخول'
+)
+translateWeb(
+  '.i18n-7', 
+  '合作方登入', 
+  'Partner Login', 
+  'Masuk Mitra', 
+  'تسجيل الشريك'
+)
+translateWeb(
+  '.i18n-8', 
+  '全球唯一HSK口语移动端考试', 
+  'The only HSK Spoken Mobile Test available worldwide', 
+  'Satu-satunya Ujian HSK Lisan Versi Seluler di Dunia', 
+  'اختبار HSK الشفوي للهواتف المحمولة الوحيد في العالم'
+)
+translateWeb(
+  '.i18n-9', 
+  '联系我们: ', 
+  'Contact Us: ', 
+  'Hubungi Kami: ', 
+  'تواصل معنا: '
+)
+translateWeb(
+  '.i18n-10', 
+  '友情链接: ', 
+  'Friendly Links: ', 
+  'Tautan Sahabat: ', 
+  'روابط صديقة: '
+)
+translateWeb(
+  '.i18n-11', 
+  '更多质讯: ', 
+  'More Information: ', 
+  'Informasi Lebih Lanjut: ', 
+  'لمزيد من المعلومات: '
+)
+translateWeb(
+  '.i18n-12', 
+  'HSK口语移动端考试App下载', 
+  'Download the HSK Spoken Mobile Test App', 
+  'Unduh Aplikasi Ujian HSK Lisan Versi Seluler', 
+  'تنزيل تطبيق اختبار HSK الشفوي للهواتف المحمولة'
+)
+translateWeb(
+  '.i18n-13', 
+  ' 请注意', 
+  ' Please note that currently', 
+  ' Harap diperhatikan bahwa saat ini', 
+  'يرجى ملاحظة أن'
+)
+translateWeb(
+  '.i18n-14', 
+  '⽬前HSK口语移动端只提供考试当⽇在中国香港、印尼、阿联酋以及澳洲的考⽣。', 
+  'The HSK Spoken mobile test is only available for students in China Hong Kong, Indonesia, the United Arab Emirates, and Australia.', 
+  'HSK Lisan versi seluler hanya tersedia untuk peserta ujian di Tiongkok Hong Kong, Indonesia, Uni Emirat Arab, dan Australia.', 
+  'يرجى ملاحظة أن HSK الشفوي للهواتف المحمولة يتوفر حاليًا فقط للطلاب الذين يجرون الاختبار في الصين (هونج كونج)، إندونيسيا، الإمارات العربية المتحدة، وأستراليا.'
+)
+translateWeb(
+  '.i18n-15', 
+  'HSK⼝语移动端考试介绍', 
+  'Introduction to HSK Spoken Mobile Test', 
+  'Pengenalan Ujian HSK Lisan Versi Seluler', 
+  'مقدمة اختبار HSK الشفوي للهواتف المحمولة'
+)
+translateWeb(
+  '.i18n-16', 
+  '欢迎来到HSK口语移动端考试中⼼', 
+  'Welcome to the HSK Spoken Mobile Test Center', 
+  'Selamat datang di Pusat Ujian HSK Lisan Versi Seluler', 
+  'مرحبًا بك في مركز اختبار HSK الشفوي للهواتف المحمولة'
+)
+translateWeb(
+  '.i18n-17', 
+  '我们利⽤最新科技，满⾜全球中⽂学习者的需求。通过考试：', 
+  'We use the latest technology to meet the needs of Chinese learners worldwide. Through the test:', 
+  'Kami menggunakan teknologi terbaru untuk memenuhi kebutuhan pembelajar bahasa Tionghoa di seluruh dunia. Melalui ujian ini:', 
+  'نحن نستخدم أحدث التكنولوجيا لتلبية احتياجات متعلمي اللغة الصينية في جميع أنحاء العالم. من خلال الاختبار:'
+)
+translateWeb(
+  '.i18n-18', 
+  '了解⾃⾝⼝语程度和不⾜，加以改进',
+  'Understand your spoken language proficiency and areas for improvement',
+  'Mengenal tingkat kemampuan berbicara anda dan meningkatkannya',
+  'فهم مستوى لغتك الشخصية ونقاط الضعف فيها، والعمل على تحسينها'
+)
+translateWeb(
+  '.i18n-19',
+  '熟悉考试流程，帮助提⾼成绩',
+  'Become familiar with the test process to help improve your scores',
+  'Familiar dengan proses ujian untuk meningkatkan hasil',
+  'التعرف على عملية الاختبار وزيادة فرص الحصول على درجات أفضل'
+)
+translateWeb(
+  '.i18n-20',
+  '获得语⾔认证，助⼒升学和求职',
+  'Obtain language certification to assist with further education and job opportunities',
+  'Mendapatkan sertifikasi bahasa untuk kemajuan belajar dan karir',
+  'الحصول على شهادة لغوية لمساعدتك في الانتقال إلى المرحلة التعليمية التالية أو البحث عن وظيفة'
+)
+translateWeb(
+  '.i18n-21',
+  '考试⽅便灵活',
+  'Convenient and Flexible Testing',
+  'Ujian yang Mudah dan Fleksibel',
+  'اختبار مريح ومرن'
+)
+
+
+translateWeb('.i18n-0', '验证码', 'Verification code', 'Kode verifikasi', 'رمز التحقق', true)
 
 $('.i18n').on('click', function() {
   let lang = $(this).attr('lang');
@@ -24,25 +161,39 @@ function urlAddParams(key, value) {
   window.location.href = url.href;
 }
 
-function translateWeb(classname, zh, en, id, ar) {
+function translateWeb(classname, zh, en, id, ar, boolean) {
   let url = new URL(window.location.href);
   let lang = url.searchParams.get('lang');
 
   switch(lang) {
     case 'zh':
-      $(classname).html(zh);
+      translate(classname, zh, boolean)
       break;
     case 'en':
-      $(classname).text(en);
+      translate(classname, en, boolean)
       break;
     case 'id':
-      $(classname).html(id);
+      translate(classname, id, boolean)
       break;
     case 'ar':
-      $(classname).html(ar);
+      translate(classname, ar, boolean)
       break;
     default:
-      $(classname).html(zh);
+      translate(classname, zh, boolean)
+  }
+}
+
+function translate(classname, lang, boolean) {
+  if (!boolean) {
+    $(classname).html(lang);
+  } else {
+    $(classname).each(function() {
+      if ($(this)[0].placeholder) {
+        $(this)[0].placeholder = lang;
+      } else {
+        $(this).html(lang);
+      }
+    });
   }
 }
 
