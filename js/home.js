c6b49d56c304fa501652337524d70341567b3939
carmenfu006 <guess_006@hotmail.com>
2023-10-18 16:38:49 +0800

    Load exam time in homepage

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 setActiveMenu()
+loadExamTime()
 
 function setActiveMenu() {
   if (window.location.hash === '') {
@@ -51,3 +52,84 @@ $('#more-faq').on('click', function() {
     $(`#card-${i}`).removeClass('d-none');
   }
 })
+
+async function loadExamTime() {
+  let lang = getSession('lang');
+  let response = await fetch('https://api.hskk.info/webapi/test_schedule/', {
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type' : 'application/json',
+        'Accept-Language': lang
+      }
+    })
+  let data = await response.json();
+  let data_elementary = data.data[0];
+  let data_intermediate = data.data[1];
+  let data_advanced = data.data[2];
+
+  const exam_time_elementary = $('#exam-time-elementary')[0];
+  const exam_time_elementary_template = document.createElement('template');
+  const exam_time_intermediate = $('#exam-time-intermediate')[0];
+  const exam_time_intermediate_template = document.createElement('template');
+  const exam_time_advanced = $('#exam-time-advanced')[0];
+  const exam_time_advanced_template = document.createElement('template');
+
+  let time_style = {
+    'timeZoneName' : 'long',
+    'year' : 'numeric',
+    'month' : 'short',
+    'day' : '2-digit',
+    'hour' : '2-digit',
+    'minute' : '2-digit'
+  }
+
+  data_elementary.forEach(function(item) {
+    let event = new Date(item.test_timestamp);
+    exam_time_elementary_template.innerHTML = `
+        <p class='card-text'>${event.toLocaleString(convertLang(lang), time_style)}</p>
+        <hr class='border-bottom'>
+    `;
+    exam_time_elementary.appendChild(exam_time_elementary_template.content);
+  });
+
+  data_intermediate.forEach(function(item) {
+    let event = new Date(item.test_timestamp);
+    exam_time_intermediate_template.innerHTML = `
+        <p class='card-text'>${event.toLocaleString(convertLang(lang), time_style)}</p>
+        <hr class='border-bottom'>
+    `;
+    exam_time_intermediate.appendChild(exam_time_intermediate_template.content);
+  });
+
+  data_advanced.forEach(function(item) {
+    let event = new Date(item.test_timestamp);
+    exam_time_advanced_template.innerHTML = `
+        <p class='card-text'>${event.toLocaleString(convertLang(lang), time_style)}</p>
+        <hr class='border-bottom'>
+    `;
+    exam_time_advanced.appendChild(exam_time_advanced_template.content);
+  });
+}
+
+function getSession(key) {
+  return sessionStorage.getItem(key) ? sessionStorage.getItem(key) : 'zh';
+}
+
+function convertLang(lang) {
+  switch(lang) {
+    case 'zh':
+      return 'zh-HK'
+      break;
+    case 'en':
+      return 'en-AU'
+      break;
+    case 'id':
+      return 'id-ID'
+      break;
+    case 'ar':
+      return 'ar-SA'
+      break;
+    default:
+      return 'zh-HK'
+  }
+}
