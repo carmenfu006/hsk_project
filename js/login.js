22ac48910d2a2db1344d21ebbdd2b0c2e54e7781
carmenfu006 <guess_006@hotmail.com>
2023-10-19 14:34:56 +0800

    Apply verification for login

--- js/login.js
+++ js/login.js
@@ -13,29 +13,45 @@ $('#to-candidate-dashboard').on('click', async(e) => {
     $('.toast').toast('show');
   } else {
 
-    let response = await fetch('https://api.hskk.info/webapi/login/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type' : 'application/json',
-        'Accept-Language': lang
-      },
-        body: JSON.stringify({
-          email: inputVal('#email'),
-          verify_code: inputVal('#verify_code')
-        })
-    })
-    let data = await response.json();
-
-    if (data.code == 200) {
-      let user_token = data.data.access;
-      $('.toast').toast('hide');
-      localStorage.setItem('user', user_token);
-      application == 'true' ? window.location.href = window.location.origin + '/application.html' : window.location.replace($('#to-candidate-dashboard')[0].form.action);
-    } else if (data.code == 400) {
-      toastMessage(data.msg)
+    if (verifyEmailVal(inputVal('#email')) == null && verifyCodeVal(inputVal('#verify_code')) == null) {
+      invalidInput('#email')
+      invalidInput('#input-code')
+      toastLang('invalid email and code')
       $('.toast').toast('show');
+    } else if (verifyEmailVal(inputVal('#email')) == null) {
+      invalidInput('#email')
+      toastLang('invalid email')
+      $('.toast').toast('show');
+    } else if (verifyCodeVal(inputVal('#verify_code')) == null) {
+      invalidInput('#input-code')
+      toastLang('invalid code')
+      $('.toast').toast('show');
+    } else if (verifyEmailVal(inputVal('#email')) != null && verifyCodeVal(inputVal('#verify_code')) != null) {
+      let response = await fetch('https://api.hskk.info/webapi/login/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type' : 'application/json',
+          'Accept-Language': lang
+        },
+          body: JSON.stringify({
+            email: inputVal('#email'),
+            verify_code: inputVal('#verify_code')
+          })
+      })
+      let data = await response.json();
+
+      if (data.code == 200) {
+        let user_token = data.data.access;
+        $('.toast').toast('hide');
+        localStorage.setItem('user', user_token);
+        application == 'true' ? window.location.href = window.location.origin + '/application.html' : window.location.replace($('#to-candidate-dashboard')[0].form.action);
+      } else if (data.code == 400) {
+        toastMessage(data.msg)
+        $('.toast').toast('show');
+      }
     }
+    
   }
 
   // fetch('/recaptcha', {
@@ -116,7 +132,8 @@ $('#verification-code-btn').on('click', async(e) => {
     timeleft -= 1;
   }, 1000);
 
-  let response = await fetch('https://api.hskk.info/webapi/send_verify_code/', {
+  if (verifyEmailVal(inputVal('#email')) != null) {
+    let response = await fetch('https://api.hskk.info/webapi/send_verify_code/', {
       method: 'POST',
       headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -127,16 +144,27 @@ $('#verification-code-btn').on('click', async(e) => {
           email: inputVal('#email')
         })
     })
-  let data = await response.json();
+    let data = await response.json();
 
-  if (data.code == 200) {
-    $('.toast').toast('hide');
-  } else if (data.code == 400) {
+    if (data.code == 200) {
+      $('.toast').toast('hide');
+    } else if (data.code == 400) {
+      timeleft = 0;
+      toastMessage(data.msg)
+      $('.toast').toast('show');
+    }
+  } else {
     timeleft = 0;
-    toastMessage(data.msg)
+    invalidInput('#email')
+    toastLang('invalid email')
     $('.toast').toast('show');
   }
 });
+
+$('#email, #verify_code').on('keyup', function() {
+  validInput('#email');
+  validInput('#verify_code');
+})
 
 function codeLang(classname) {
   switch(lang) {
@@ -214,6 +242,63 @@ function toastLang(error_type) {
         toastMessage(error_type, '', '')
     }
   }
+
+  if (error_type == 'invalid email') {
+    switch(lang) {
+      case 'zh':
+        toastMessage('请输入合法的邮件地址。', '', '')
+        break;
+      case 'en':
+        toastMessage('Please enter a valid email address.', '', '')
+        break;
+      case 'id':
+        toastMessage('Silakan isi alamat email.', '', '')
+        break;
+      case 'ar':
+        toastMessage('يرجى إدخال عنوان بريد إلكتروني صالح.', '', '')
+        break;
+      default:
+        toastMessage('请输入合法的邮件地址。', '', '')
+    }
+  }
+
+  if (error_type == 'invalid code') {
+    switch(lang) {
+      case 'zh':
+        toastMessage('请确保验证码至少包含 6 个字符。', '', '')
+        break;
+      case 'en':
+        toastMessage('Please make sure the verification code contains at least 6 characters.', '', '')
+        break;
+      case 'id':
+        toastMessage('Harap pastikan kode verifikasi berisi minimal 6 karakter.', '', '')
+        break;
+      case 'ar':
+        toastMessage('يرجى التأكد من أن رمز التحقق يحتوي على 6 أحرف على الأقل.', '', '')
+        break;
+      default:
+        toastMessage('请确保验证码至少包含 6 个字符。', '', '')
+    }
+  }
+
+  if (error_type == 'invalid email and code') {
+    switch(lang) {
+      case 'zh':
+        toastMessage('请输入合法的邮件地址。', '请确保验证码至少包含 6 个字符。', '')
+        break;
+      case 'en':
+        toastMessage('Please enter a valid email address.', 'Please make sure the verification code contains at least 6 characters.', '')
+        break;
+      case 'id':
+        toastMessage('Silakan isi alamat email.', 'Harap pastikan kode verifikasi berisi minimal 6 karakter.', '')
+        break;
+      case 'ar':
+        toastMessage('يرجى إدخال عنوان بريد إلكتروني صالح.', 'يرجى التأكد من أن رمز التحقق يحتوي على 6 أحرف على الأقل.', '')
+        break;
+      default:
+        toastMessage('请输入合法的邮件地址。', '请确保验证码至少包含 6 个字符。', '')
+    }
+  }
 }
 
 function toastMessage(line1, line2, line3) {
@@ -236,4 +321,20 @@ function getSession(key) {
 
 function inputVal(id) {
   return $(id).val()
+}
+
+function verifyEmailVal(email) {
+  return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+}
+
+function verifyCodeVal(code) {
+  return code.match(/^(\s*\d{6}\s*)(,\s*\d{6}\s*)*,?\s*$/);
+}
+
+function invalidInput(id) {
+  $(id).addClass('invalid');
+}
+
+function validInput(id) {
+  $(id).removeClass('invalid');
 }
