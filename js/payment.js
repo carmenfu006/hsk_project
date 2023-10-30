0c15bfc28380b5f80662c648b26c11f02eb5cd65
carmenfu006 <guess_006@hotmail.com>
2023-10-30 14:59:34 +0800

    Limit payment for status null

--- js/payment.js
+++ js/payment.js
@@ -22,37 +22,42 @@ window.addEventListener('DOMContentLoaded', async () => {
     let info = data.data;
 
     if (data.code == 200) {
-      const clientSecret = payment_id;
-      $('#selected-exam-level').html(displaySelectedExam(info.test_level));
-      $('.total-amount').html((info.payment_amount/100).toFixed(2));
-      $('.currency-display').html((info.payment_currency).toUpperCase());
-      const elements = stripe.elements({ clientSecret });
-      const paymentElement = elements.create('payment');
-      paymentElement.mount('#payment-element');
+      if (info.payment_status != null) {
+        $('#payment-section').hide();
+        $('#payment-link-invalid').show();
+      } else {
+        const clientSecret = payment_id;
+        $('#selected-exam-level').html(displaySelectedExam(info.test_level));
+        $('.total-amount').html((info.payment_amount/100).toFixed(2));
+        $('.currency-display').html((info.payment_currency).toUpperCase());
+        const elements = stripe.elements({ clientSecret });
+        const paymentElement = elements.create('payment');
+        paymentElement.mount('#payment-element');
 
-      const form = $('#payment-form');
-      form.on('submit', async (e) => {
-        e.preventDefault();
-        $('#payment-btn').attr('disabled', true);
+        const form = $('#payment-form');
+        form.on('submit', async (e) => {
+          e.preventDefault();
+          $('#payment-btn').attr('disabled', true);
 
-        const {error} = await stripe.confirmPayment({
-          elements,
-          confirmParams: {
-            return_url: window.location.origin + '/payment-success.html'
+          const {error} = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+              return_url: window.location.origin + '/payment-success.html'
+            }
+          })
+          if (error) {
+            $('.toast').toast('show');
+            $('#payment-btn').attr('disabled', false);
           }
         })
-        if (error) {
-          $('.toast').toast('show');
-          $('#payment-btn').attr('disabled', false);
-        }
-      })
 
-      if (info.coupon_code != null) {
-        $('#discount-code').prop('readonly', true);
-        $('#discount-code').val(info.coupon_code);
-        $('#discount-error-message').addClass('d-none');
-        $('#discount-success-message').removeClass('d-none');
-        $('#verify-code').prop('disabled', true);
+        if (info.coupon_code != null) {
+          $('#discount-code').prop('readonly', true);
+          $('#discount-code').val(info.coupon_code);
+          $('#discount-error-message').addClass('d-none');
+          $('#discount-success-message').removeClass('d-none');
+          $('#verify-code').prop('disabled', true);
+        }
       }
     }
   }
