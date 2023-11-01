c7c983a47445e1b7ed58b53493e9efd2a812f76c
carmenfu006 <guess_006@hotmail.com>
2023-11-01 14:04:00 +0800

    Change payment to use javascript only

--- js/payment.js
+++ js/payment.js
@@ -7,8 +7,8 @@ window.addEventListener('DOMContentLoaded', async () => {
   const stripe = Stripe('pk_live_51NxVaQArZBHZvCDsIip5DutxzIoCQZ4DIXwNxLZtWiMb2bKBPp7eCH7d4bp1vSvBs8CCdzItKEbaQqn8qTgXz23N00i4I9qDpu', { locale : `${lang}` })
 
   if (payment_id == null || payment_id == '') {
-    $('#payment-section').hide();
-    $('#payment-link-invalid').show();
+    document.getElementById('payment-section').style.display = 'none';
+    document.getElementById('payment-link-invalid').style.display = 'block';
   } else {
     $('#payment-link-invalid').hide();
     let response = await fetch(`https://api.hskk.org/webapi/order_read_payment/${paymentIntentId}`, {
@@ -20,24 +20,31 @@ window.addEventListener('DOMContentLoaded', async () => {
     })
     let data = await response.json();
     let info = data.data;
+    let total_amounts = document.querySelectorAll('.total-amount');
+    let currency_displays = document.querySelectorAll('.currency-display');
 
     if (data.code == 200) {
       if (info.payment_status != null) {
-        $('#payment-section').hide();
-        $('#payment-link-invalid').show();
+        document.getElementById('payment-section').style.display = 'none';
+        document.getElementById('payment-link-invalid').style.display = 'block';
       } else {
         const clientSecret = payment_id;
-        $('#selected-exam-level').html(displaySelectedExam(info.test_level));
-        $('.total-amount').html((info.payment_amount/100).toFixed(2));
-        $('.currency-display').html((info.payment_currency).toUpperCase());
+        document.getElementById('selected-exam-level').innerHTML = displaySelectedExam(info.test_level);
+        total_amounts.forEach(total_amount => {
+          total_amount.innerHTML = (parseInt(info.payment_amount)/100).toFixed(2);
+        });
+        currency_displays.forEach(currency_display => {
+          currency_display.innerHTML = (info.payment_currency).toUpperCase();
+        });
         const elements = stripe.elements({ clientSecret });
         const paymentElement = elements.create('payment');
         paymentElement.mount('#payment-element');
 
-        const form = $('#payment-form');
-        form.on('submit', async (e) => {
+        const form = document.getElementById('payment-form');
+        
+        form.addEventListener('submit', async (e) => {
           e.preventDefault();
-          $('#payment-btn').attr('disabled', true);
+          document.getElementById('payment-btn').disabled = true;
 
           const {error} = await stripe.confirmPayment({
             elements,
@@ -47,38 +54,38 @@ window.addEventListener('DOMContentLoaded', async () => {
           })
           if (error) {
             $('.toast').toast('show');
-            $('#payment-btn').attr('disabled', false);
+            document.getElementById('payment-btn').disabled = false;
           }
         })
 
         if (info.coupon_code != null) {
-          $('#discount-code').prop('readonly', true);
-          $('#discount-code').val(info.coupon_code);
-          $('#discount-error-message').addClass('d-none');
-          $('#discount-success-message').removeClass('d-none');
-          $('#verify-code').prop('disabled', true);
+          document.getElementById('discount-code').readOnly = true;
+          document.getElementById('discount-code').value = info.coupon_code;
+          document.getElementById('discount-error-message').classList.add('d-none');
+          document.getElementById('discount-success-message').classList.remove('d-none');
+          document.getElementById('verify-code').disabled = true;
         }
       }
     }
   }
 
   // Discount and update paymentIntent
-  const verify_code = $('#verify-code');
-  const discount_field = $('#discount-code');
+  const verify_code = document.getElementById('verify-code');
+  const discount_field = document.getElementById('discount-code');
   
-  discount_field.on('keyup', function() {
-    if (discount_field.val() != '') {
-      verify_code.attr('disabled', false);
+  discount_field.addEventListener('keyup', function() {
+    if (discount_field.value != '') {
+      verify_code.disabled = false;
     } else {
-      verify_code.attr('disabled', true);
+      verify_code.disabled = true;
     }
   })
 
-  verify_code.on('click', async (e) => {
+  verify_code.addEventListener('click', async (e) => {
     e.preventDefault();
-    verify_code.html('<i class="fa-solid fa-spinner fa-spin"></i>')
+    verify_code.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i>';
 
-    let discount_code = $('#discount-code').val();
+    let discount_code = document.getElementById('discount-code').value;
 
     let response = await fetch(`https://api.hskk.org/webapi/order_read_payment/${paymentIntentId}`, {
       method: 'PUT',
@@ -92,17 +99,22 @@ window.addEventListener('DOMContentLoaded', async () => {
       })
     })
     let data = await response.json();
+    let total_amounts = document.querySelectorAll('.total-amount');
+    let discount_error_message = document.getElementById('discount-error-message');
+    let discount_success_message = document.getElementById('discount-success-message');
 
     if (data.code == 200) {
       let info = data.data;
-      $('.total-amount').html((parseInt(info.payment_amount)/100).toFixed(2))
+      total_amounts.forEach(total_amount => {
+        total_amount.innerHTML = (parseInt(info.payment_amount)/100).toFixed(2);
+      });
       applyText(lang, verify_code, '应用', 'Apply', 'Aplikasi', 'تطبيق');
-      verify_code.css('background-color', '#3FD3C3')
-      $('#discount-error-message').addClass('d-none')
-      $('#discount-success-message').removeClass('d-none')
+      verify_code.style.backgroundColor = '#3FD3C3';
+      discount_error_message.classList.add('d-none')
+      discount_success_message.classList.remove('d-none')
     } else {
-      $('#discount-error-message').removeClass('d-none')
-      $('#discount-success-message').addClass('d-none')
+      discount_error_message.classList.remove('d-none')
+      discount_success_message.classList.add('d-none')
       applyText(lang, verify_code, '应用', 'Apply', 'Aplikasi', 'تطبيق');
     }
     checkPaymentAmount()
@@ -114,13 +126,13 @@ window.addEventListener('DOMContentLoaded', async () => {
   
   function applyText(lang, target, zh_hans, en, id, ar) {
     if (lang == null || lang == 'zh-hans' || lang == 'zh-hant') {
-      target.html(zh_hans);
+      target.innerHTML = zh_hans;
     } else if (lang == 'en') {
-      target.html(en);
+      target.innerHTML = en
     } else if (lang == 'id') {
-      target.html(id);
+      target.innerHTML = id
     } else if (lang == 'ar') {
-      target.html(ar);
+      target.innerHTML = ar
     }
   }
 
@@ -177,13 +189,18 @@ window.addEventListener('DOMContentLoaded', async () => {
     let data = await response.json();
     let info = data.data;
 
+    let verify_code = document.getElementById('verify-code');
+    let discount_code = document.getElementById('discount-code');
+    let discount_error_message = document.getElementById('discount-error-message');
+    let discount_success_message = document.getElementById('discount-success-message');
+
     if (data.code == 200) {
       if (info.coupon_code != null) {
-        $('#discount-code').prop('readonly', true);
-        $('#discount-code').val(info.coupon_code);
-        $('#discount-error-message').addClass('d-none');
-        $('#discount-success-message').removeClass('d-none');
-        $('#verify-code').prop('disabled', true);
+        discount_code.readOnly = true;
+        discount_code.value = info.coupon_code;
+        discount_error_message.classList.add('d-none');
+        discount_success_message.classList.remove('d-none');
+        verify_code.disabled = true;
       }
     }
   }
